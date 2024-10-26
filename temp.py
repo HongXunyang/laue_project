cda438e4abae5e70afb0a014de64818a77691eb7
Xunyang Hong <hongxunyang@qq.com>
2024-10-26 16:24:57 +0200

    cleared up

--- temp.py
+++
@@ -1,7 +0,0 @@
-import json
-from to_cad import sampleholder_dict_to_cad
-
-# read json file
-with open("../data/sampleholder.json", "r") as f:
-    sampleholder_dict = json.load(f)
-sampleholder_dict_to_cad(sampleholder_dict, cad_file="test.stl")
